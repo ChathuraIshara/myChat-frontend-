3563d9a18f58b1a0d48dbd4bb5a4382c1b5e3cfd
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-18 12:37:23 +0530

    cloudinary changed

--- src/components/SettingsPopup/SettingsPopup.js
+++ src/components/SettingsPopup/SettingsPopup.js
@@ -56,7 +56,7 @@ export default function SettingsPopup({
     formData.append("file",imgFile);
     formData.append("upload_preset",preset_key);
 
-    axios.post('https://api.cloudinary.com://553964132456753:TruyFpN0DKLdzMY3G4X6livN0mU@dcabglcnt',formData).then(res=>console.log(res)).catch(err=>console.log(err));
+    axios.post('https://api.cloudinary.com/v1_1/dcabglcnt/image/upload',formData).then(res=>console.log(res)).catch(err=>console.log(err));
 
 
     try{
